ede0e496fd4131ed86a896291545e208e80b97cf
xiaotiandada <95282399@qq.com>
2018-08-07 17:14:40 +0800

    新增订单页面
    增加无订单时的订单页面

--- src/pages/order/index.vue
+++ src/pages/order/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="">
-    order
+  <div class="order">
+    <div class="orderImg">
+      <img src="/static/img/ndd.png" alt="ndd">
+    </div>
+    <p class="orderTitle">当前没有订单！</p>
   </div>
 </template>
 
@@ -15,5 +18,27 @@
 </script>
 
 <style lang="less" scoped>
-
+.order{
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: #f4f4f4;
+}
+  .orderImg {
+    width: 130rpx;
+    height: 120rpx;
+    margin: 460rpx auto 0;
+    img{
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .orderTitle{
+    text-align: center;
+    font-size: 16px;
+    color: #aaaaaa;
+    margin-top: 40rpx;
+  }
 </style>
